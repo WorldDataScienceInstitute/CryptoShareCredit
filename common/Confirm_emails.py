ac5525bc58f1a4df038d1ec25a0d86123b36326e
Christopher Rutherford <chris36787@gmail.com>
2021-12-05 22:57:19 -0500

    tiny formatting changes

--- common/Confirm_emails.py
+++ common/Confirm_emails.py
@@ -3,6 +3,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from django.conf import settings
 
+
 def Card_Creation_Email(to_addr):
     port = settings.EMAIL_PORT
     msg = MIMEMultipart('alternative')
@@ -14,18 +15,17 @@ def Card_Creation_Email(to_addr):
     msg['From'] = f"Crypto$hare <{settings.EMAIL_HOST_USER}>"
     msg['To'] = to_addr
     html = """
-  <html>
-  <head></head>
+    <html>
+    <head></head>
     <body>
-    
-    <p>Hi there!</p>
+        <p>Hi there!</p>
 
-      <p>This email has been used to create a debit card at <a href="http://www.cryptoshareapp.com/">Crypto$hare</a> - The First Peer to Peer Lending Platform that allows CryptoCurrency & Physical assets to be used as Collateral! </p>
+        <p>This email has been used to create a debit card at <a href="http://www.cryptoshareapp.com/">Crypto$hare</a> - The First Peer to Peer Lending Platform that allows CryptoCurrency & Physical assets to be used as Collateral! </p>
 
-      <p>If this was not you, please secure your account at <a href="http://www.cryptoshareapp.com/">Crypto$hare</a></p>
+        <p>If this was not you, please secure your account at <a href="http://www.cryptoshareapp.com/">Crypto$hare</a></p>
     </body>
-  </html>
-  """
+    </html>
+    """
 
     part1 = MIMEText(html, 'html')
     part2 = MIMEText("This email has been used to create a debit card at  http://www.cryptoshareapp.com/", 'Crypto$share')
@@ -42,7 +42,6 @@ def Card_Creation_Email(to_addr):
     s.quit()
     return
 
-# Card_Creation_Email("noreply.Cryptoshare@gmail.com")
 
 def Account_Creation_Email(to_addr):
     port = settings.EMAIL_PORT
@@ -82,9 +81,9 @@ def Account_Creation_Email(to_addr):
     s.quit()
     return
 
-def Code_Creation_Email(to_addr,pin):
+
+def Code_Creation_Email(to_addr, pin):
     port = settings.EMAIL_PORT
-    htmlpin=pin
     msg = MIMEMultipart('alternative')
     msg['Subject'] = "Crypto$hare Confirmation Pin"
     #Code worked on stmp with gmail but not from domain.com
@@ -93,7 +92,7 @@ def Code_Creation_Email(to_addr,pin):
     msg['From'] = f"Crypto$hare <{settings.EMAIL_HOST_USER}>"
     msg['To'] = to_addr
     
-    html = """
+    html = f"""
     <html>
     <head></head>
         <body>
@@ -104,12 +103,12 @@ def Code_Creation_Email(to_addr,pin):
 
         <p>Your registered 6 digit pin for Crypto$hare is the following:</p>
 
-        <big><big><big><big><b>%s</b></big></big></big></big>
+        <big><big><big><big><b>{pin}</b></big></big></big></big>
 
         <p>If this was not you, please secure your account at <a href="http://www.cryptoshareapp.com/">Crypto$hare</a></p>
         </body>
     </html>
-    """%(pin)
+    """
 
     part1 = MIMEText(html, 'html')
     part2 = MIMEText("This email has been used to create an account at  http://www.cryptoshareapp.com/", 'Crypto$share')
@@ -125,5 +124,3 @@ def Code_Creation_Email(to_addr,pin):
     s.sendmail(settings.EMAIL_HOST_USER, to_addr, msg.as_string())
     s.quit()
     return
-
- 
