04c353c0a7486d5df8c671b29fc1e39314818d1b
ANVRRT <albertonavarreteramirez@gmail.com>
2022-01-03 20:20:42 -0600

    CryptoApis Verification URL

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 from . import views
 
 app_name = 'authentication'
@@ -12,4 +13,6 @@ urlpatterns = [
     path('PasswordReset', views.reset_password, name="PasswordReset"),
     path('Login/', views.views.login, name='Login'),
     path('Logout/', views.logout, name='Logout'),
+    path('cryptoapis-cb-97229b2e3726a10a5585eb06f4eaaa064cda8246aae5848fa9a0c25193686255.txt/',TemplateView.as_view(template_name="cryptoapis-cb-97229b2e3726a10a5585eb06f4eaaa064cda8246aae5848fa9a0c25193686255.txt", content_type="text/plain"), name="CryptoApis")
+
 ]
