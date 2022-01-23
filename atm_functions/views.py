19f553a3c2bffd6878a08cb50a5f77ec9aa2f1d9
ANVRRT <albertonavarreteramirez@gmail.com>
2022-01-23 02:30:25 -0600

    Changed webhook bytes reader function

--- atm_functions/views.py
+++ atm_functions/views.py
@@ -778,8 +778,8 @@ def confirmed_coin_transactions(request):
 
         # print(request.headers)
 
-        bpayload = request_reader.stream.read1()  # UNCOMMENT FOR LOCAL TESTING
-        # bpayload = request_reader.read() #UNCOMMENT FOR PRODUCTION
+        # bpayload = request_reader.stream.read1()  # UNCOMMENT FOR LOCAL TESTING ENVIRRONMENT
+        bpayload = request_reader.read() #UNCOMMENT FOR PRODUCTION ENVIRONMENT
 
         payload = bpayload.decode("utf-8")
 
