842d859aa996ac05e111ce1616f2ca24b7756d28
ANVRRT <albertonavarreteramirez@gmail.com>
2022-01-31 02:11:36 -0600

    Several changes to create borrowing offer

--- static/js/create_offers.js
+++ static/js/create_offers.js
@@ -1,19 +1,33 @@
 function SetMaxAmount(getField, setField){
-    var maxAmount = document.getElementById(getField).value.split('_')[1];
-    document.getElementById(setField).max = parseFloat(maxAmount);
+    var currency = document.getElementById(getField).value.split(' ')[0];
+    var rate = document.getElementById(currency+"_exchangeRate").innerHTML;
+    var maxAmount = 1*1000/rate
+    var minAmount = 1*150/rate
+    // var maxAmount = document.getElementById(getField).value.split(' ')[0];
+    
+    document.getElementById(setField).value = "";
+
+    document.getElementById(setField).max = parseFloat(maxAmount).toFixed(4);
+    document.getElementById(setField).min = parseFloat(minAmount).toFixed(4);
+    document.getElementById(setField).placeholder = "Enter the amount you want to borrow || Min: " + minAmount.toFixed(4) +" Max: " + maxAmount.toFixed(4);
     document.getElementById(setField).removeAttribute("readonly");
 }
 
-function SetCollateral(getAmountField, getCollateralCurrency, setCollateralField){
-    var currency = document.getElementById(getCollateralCurrency).value.split(' ')[0];
-    alert(currency);
+function SetCollateral(getAmountField, getPaymentCurrency, getCollateralCurrency, setCollateralField){
+    var currency = document.getElementById(getPaymentCurrency).value.split(' ')[0];
+    var currencyCollateral = document.getElementById(getCollateralCurrency).value.split(' ')[0];
+
     if(currency != "NotSelected"){
-        var currencyExchangeRate = document.getElementById(currency+"_exchangeRate").innerHTML;
+        var currencyRate = document.getElementById(currency+"_exchangeRate").innerHTML;
+        var currencyCollateralRate = document.getElementById(currencyCollateral+"_exchangeRate").innerHTML;
+
         var loan_amount = document.getElementById(getAmountField).value;
+
+        var currencyLoanExchange = loan_amount * currencyRate;
+
+        var collateral = currencyLoanExchange / currencyCollateralRate * 1.2;
     
-        var collateral = (loan_amount / parseFloat(currencyExchangeRate))*1.2;
-    
-        document.getElementById(setCollateralField).value = collateral.toFixed(4);
+        document.getElementById(setCollateralField).value = parseFloat(collateral).toFixed(4);
     }
 
     
