718ae0a81c0e807d5e5a6936eea5e14ec18dbe85
ANVRRT <albertonavarreteramirez@gmail.com>
2022-03-26 16:28:56 -0600

    Removed scheduled test task

--- ATM/celery.py
+++ ATM/celery.py
@@ -23,10 +23,10 @@ app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
 
-@app.on_after_configure.connect
-def setup_periodic_tasks(sender, **kwargs):
-    # Calls test('hello') every 10 seconds.
-    sender.add_periodic_task(5.0, test.s('hello'), name='add every 10')
+# @app.on_after_configure.connect
+# def setup_periodic_tasks(sender, **kwargs):
+#     # Calls test('hello') every 10 seconds.
+#     sender.add_periodic_task(5.0, test.s('hello'), name='add every 10')
 
 
 @app.task
