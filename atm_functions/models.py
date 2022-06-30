4550bac84aa30f6e06394d1732522fc2b5bb641e
ANVRRT <albertonavarreteramirez@gmail.com>
2022-06-29 23:47:26 -0500

    Added Estate feature

--- atm_functions/models.py
+++ atm_functions/models.py
@@ -17,6 +17,15 @@ class Account(models.Model):
     state = models.CharField(max_length=57, null=True)
     birthdate = models.DateField(null=True)
     username = models.CharField(max_length=57, unique=True, null=True)
+    net_worth = models.DecimalField(max_digits=20, decimal_places=2, default=0) 
+
+class UserAssets(models.Model):
+    id_asset = models.AutoField(primary_key=True)
+    email = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    type = models.CharField(max_length=15, null=True)
+    name = models.CharField(max_length=255, null=True)
+    worth = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    extra_field = models.CharField(max_length=57, null=True)
 
 class Business(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
