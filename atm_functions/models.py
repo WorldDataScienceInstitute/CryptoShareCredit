f54ff7fd9fb5daad80b8c1e1dc4822f78e5d342e
ANVRRT <albertonavarreteramirez@gmail.com>
2022-08-13 20:09:26 -0500

    Minor changes

--- atm_functions/models.py
+++ atm_functions/models.py
@@ -1,7 +1,6 @@
 from symtable import Symbol
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.base import Model
 from django.utils import timezone
 from datetime import datetime, timedelta
 
