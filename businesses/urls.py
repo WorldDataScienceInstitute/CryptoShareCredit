3178c100f1f6b6e753f5055e3c4482751af099bd
ANVRRT <albertonavarreteramirez@gmail.com>
2022-08-14 21:09:36 -0500

    Migrated Business into another app

--- businesses/urls.py
+++ businesses/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = "businesses"
 
 urlpatterns = [
-    path("", views.test, name="Businesses"),
+    path("", views.businesses, name="Businesses"),
+    path("create/", views.create_business, name="Create"),
+    path("manage/", views.manage_businesses, name="Manage"),
+    path("edit/", views.edit_business, name="Edit"),
+    path("search/", views.search_business, name="Search"),
 ]
